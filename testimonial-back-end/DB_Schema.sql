238b2e6d43d38f6ee7059cebf9c550a10957ab26
vibhavmadkaikar <madkaikarvibhav.31@gmail.com>
2025-02-03 18:14:51 +0530

    userid field added to the testimonials table

--- testimonial-back-end/DB_Schema.sql
+++ testimonial-back-end/DB_Schema.sql
@@ -1,44 +1,48 @@
 CREATE DATABASE testimonialsdb;
 
-USE Cascading;
-
 USE testimonialsdb;
 
 CREATE TABLE users (
-	userid INT AUTO_INCREMENT,
+    userid INT AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     domain VARCHAR(255) NOT NULL,
     
-    CONSTRAINT pk_userid PRIMARY KEY(userid)
-    );
+    CONSTRAINT pk_userid PRIMARY KEY (userid)
+);
     
-    CREATE TABLE customers
-( 
-	testimonialid INT,
+CREATE TABLE customers (
     customerid INT AUTO_INCREMENT,
-    password VARCHAR(255) NOT NULL,
+    customername VARCHAR(255) NOT NULL,
     customeremail VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     
     CONSTRAINT pk_customer_id PRIMARY KEY (customerid)
 );
    
 
-CREATE TABLE testimonials
-(
+CREATE TABLE testimonials (
 	testimonialid INT AUTO_INCREMENT,
+    userid INT,
     rating INT DEFAULT 0,
+    customerid INT,
     customername VARCHAR(255) NOT NULL,
-    picture BLOB,
-    description VARCHAR(255) NOT NULL,
     customerabout VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     createdat DATETIME NOT NULL,
     isvisible BOOL,
-	
+    
     CONSTRAINT pk_testimonial_id PRIMARY KEY (testimonialid),
-    CONSTRAINT fk_customer_id FOREIGN KEY (customerid) REFERENCES customers (customerid) ON DELETE CASCADE
-    );
+    
+    CONSTRAINT fk_customer_id FOREIGN KEY (customerid)
+        REFERENCES customers (customerid)
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_user_id FOREIGN KEY (userid)
+        REFERENCES users (userid)
+        ON DELETE CASCADE
+);
 	
     
